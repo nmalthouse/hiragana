a9834066fe8b0f32d022ca853b5fcabde62872d4
Niklas <102893782+nmalthouse@users.noreply.github.com>
2025-02-28 21:46:19 -0600

    improve reading args

--- reading.py
+++ reading.py
@@ -3,11 +3,19 @@ import json
 import subprocess
 import random
 import corpus
+import argparse
 # requires mpv to be installed and in path
 # How it works
 # It displays the kana and english translation for a random word.
 # You sound out the word first, then type.
 # Hitting enter with a blank line will play the associated mp3 file to check pronunciation
+default_kana = corpus.all_hiragana
+parser = argparse.ArgumentParser( description="Practice reading hiragana")
+parser.add_argument("--count", type=int, default = 50)
+parser.add_argument("--minkana", type=int, help="If specified, only drill words with atleast MINKANA kana", default=1)
+parser.add_argument('--include' , type=str, default=default_kana, help="A string specifying the hiragana groups to drill, default is: " + default_kana + " To drill just words with vowels and K's do --include AK")
+
+args = parser.parse_args()
 
 con = sqlite3.connect("anki_cards/collection.anki2")
 cur = con.cursor()
@@ -16,17 +24,14 @@ res = cur.execute("SELECT flds FROM notes")
 mp3map = json.load(open("anki_cards/media"))
 inv_map = {v: k for k, v in mp3map.items()}
 
-min_kana_count = 4
-ex_count = 25
+min_kana_count = args.minkana
 do_all = False
 
-kana = corpus.new_corp
-allchars = ""
-for item in kana:
-    allchars += item[1]
+allchars = corpus.getAggregateString(args.include)
 
 vocab_list = []
 count = 0
+prob_count = 0
 for item in res:
     sections = item[0].split("\x1f")
     # 0 pkey
@@ -37,14 +42,18 @@ for item in res:
     # 5 sound mp3, within []
     if len(sections[3]) >= min_kana_count and (do_all or all(ele in allchars for ele in sections[3])):
         count += 1
-        mp3_name = inv_map[sections[5].split(':')[1][:-1]]
+        sound_name = sections[5].split(':')
+        if len(sound_name) != 2:
+            prob_count += 1
+            continue
+        mp3_name = inv_map[sound_name[1][:-1]]
         vocab_list.append({'kana': sections[3], 'mp3':mp3_name, 'english': sections[4]})
     #subprocess.run(['mpv', 'anki_cards/' + mp3_name], capture_output = True)
     #break
 
-print("num words ", count)
+print("num words: ", count, " Prob words: ", prob_count)
 
-for i in range(0, ex_count):
+for i in range(0, args.count):
     item = vocab_list[random.randrange(len(vocab_list))]
 
     print(item['english'],item['kana'])
--- readme.txt
+++ readme.txt
@@ -1,13 +1,14 @@
-Small python programs to help learn hiragana
-
-
+Small python programs to help learn Hiragana
 
 symb.py is the most basic start with:
-python symb.py --include A #Start with learning the vowels
+python symb.py --include A # Start with learning the vowels
 
 vocab.py drills typing words
 
 reading.py drills words using an anki deck. An audio file can be played by hitting enter without any input
+reading.py requires mpv be installed and in your path
+
+run with --help on any of the programs to see options
 
 Download the anki deck with:
 https://ankiweb.net/shared/info/2141233552
@@ -16,4 +17,4 @@ using zip:
 
 unzip my_deck.apkg -d anki_cards
 
-python reading.py
+python reading.py --minkana 4 --count 50 # Drill words with atleast 4 kana
